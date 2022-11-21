aeec406a2639372b28b92ac1644f288ead2c2781
BogartDME <edwin.pietrowski@gmail.com>
2022-11-21 18:53:15 -0500

    accepted changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,9 @@
 $ .ajax({
     type:"GET",
-    url:"https://app.ticketmaster.com/discovery/v2/events.json?keyword=coldwarkids&countryCode=US&limit=10&apikey=hMNJM8tD1PpcNAUBm6ZArJQt8MPJUpWy",
+    //when spotify api is up this api will be dependent on pulling the performer to search their events as such:
+    // "https://app.ticketmaster.com/discovery/v2/events.json?keyword=" + "&countryCode=US&size=10&apikey={}"
+    //API Key is stored in .gitignor
+    url:"https://app.ticketmaster.com/discovery/v2/events.json?keyword=Adele-&countryCode=US&size=10&apikey=hMNJM8tD1PpcNAUBm6ZArJQt8MPJUpWy",
     async:true,
     dataType: "json",
     success: function(json) {
