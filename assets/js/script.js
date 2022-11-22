5f7b37077d1019050ac21dc2be69fd7f52dcc0a3
BogartDME <edwin.pietrowski@gmail.com>
2022-11-21 19:24:28 -0500

    minor changes to js with api. gitignor not needed

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
 $ .ajax({
     type:"GET",
     //when spotify api is up this api will be dependent on pulling the performer to search their events as such:
-    // "https://app.ticketmaster.com/discovery/v2/events.json?keyword=" + "&countryCode=US&size=10&apikey={}"
+    // "https://app.ticketmaster.com/discovery/v2/events.json?keyword=" + "&countryCode=US&size=10&apikey=hMNJM8tD1PpcNAUBm6ZArJQt8MPJUpWy"
     //API Key is stored in .gitignor
     url:"https://app.ticketmaster.com/discovery/v2/events.json?keyword=Adele-&countryCode=US&size=10&apikey=hMNJM8tD1PpcNAUBm6ZArJQt8MPJUpWy",
     async:true,
@@ -10,10 +10,10 @@ $ .ajax({
         console.log(json);
             // Parse the response.
         // console.log(response.status); 
-             },
+    },
     error: function(xhr, status, err) {
-                // This time, we do not end up here!
-             }
-  });
+    }
+
+});
 
 
