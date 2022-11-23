735863f16d3fe58e7bf7b4baef39229f617374de
BogartDME <edwin.pietrowski@gmail.com>
2022-11-22 22:22:40 -0500

    cleaned up js

--- assets/js/script.js
+++ assets/js/script.js
@@ -28,23 +28,7 @@ function eventResults() {
   });
 }
 eventResults();
-$ .ajax({
-    type:"GET",
-    //when spotify api is up this api will be dependent on pulling the performer to search their events as such:
-    // "https://app.ticketmaster.com/discovery/v2/events.json?keyword=" + "&countryCode=US&size=10&apikey=hMNJM8tD1PpcNAUBm6ZArJQt8MPJUpWy"
-    //API Key is stored in .gitignor
-    url:"https://app.ticketmaster.com/discovery/v2/events.json?keyword=Adele-&countryCode=US&size=10&apikey=hMNJM8tD1PpcNAUBm6ZArJQt8MPJUpWy",
-    async:true,
-    dataType: "json",
-    success: function(json) {
-        console.log(json);
-            // Parse the response.
-        // console.log(response.status); 
-    },
-    error: function(xhr, status, err) {
-    }
 
-});
 
 // save comments to local storage 
 $("#save-btn").on('click',function () {
