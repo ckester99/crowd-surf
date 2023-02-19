14dc32e0adc978efd87a5e6081c01c7061551614
maiaferrer <mferrer2@uncc.edu>
2023-02-19 17:44:49 -0500

    debugged the spotify api and updated some stying

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Crowd Surf
-  <a id="readme-top"></a>
-  
-  
+
+<a id="readme-top"></a>
+
   <details close> 
   <summary> Table of Contents</summary><br/>
   
@@ -15,61 +15,41 @@
   - [Contributions](#contributions)
   - [Contact](#contact)
   - [Future Features](#future-features)
-  
+
 
   </details>
 
-  ## License
-  
-  [![License: Apache License v2.0](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)
-    
-  
+## License
+
+[![License: Apache License v2.0](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)
 
 ---
 
 ## About the Project
 
-  Hi, we are the Velvet Players, thank you for your interest in our project! Crowd Surf is a dashboard that allows the user to view top streaming songs of their favorite artists. Crowd Surf also provides an option for users to search for upcoming shows from the artists the users search.
+Hi, we are the Velvet Players, thank you for your interest in our project! Crowd Surf is a dashboard that allows the user to view top streaming songs of their favorite artists. Crowd Surf also provides an option for users to search for upcoming shows from the artists the users search.
 
-  
+## Built With
 
-  ## Built With 
+![CSS3](https://img.shields.io/badge/css3-%231572B6.svg?style=for-the-badge&logo=css3&logoColor=white)![HTML5](https://img.shields.io/badge/html5-%23E34F26.svg?style=for-the-badge&logo=html5&logoColor=white)![TailwindCSS](https://img.shields.io/badge/tailwindcss-%2338B2AC.svg?style=for-the-badge&logo=tailwind-css&logoColor=white)![JavaScript](https://img.shields.io/badge/javascript-%23323330.svg?style=for-the-badge&logo=javascript&logoColor=%23F7DF1E)
 
-  ![CSS3](https://img.shields.io/badge/css3-%231572B6.svg?style=for-the-badge&logo=css3&logoColor=white)![HTML5](https://img.shields.io/badge/html5-%23E34F26.svg?style=for-the-badge&logo=html5&logoColor=white)![TailwindCSS](https://img.shields.io/badge/tailwindcss-%2338B2AC.svg?style=for-the-badge&logo=tailwind-css&logoColor=white)![JavaScript](https://img.shields.io/badge/javascript-%23323330.svg?style=for-the-badge&logo=javascript&logoColor=%23F7DF1E)
+## Visuals
 
-    
-  ## Visuals
-  
-  
-
-
-  ![Alt full site image](./assets/images/Screen%20Shot%202023-01-03%20at%2010.29.35%20PM.png)
-
-
-
+![Alt full site image](./assets/images/Screen%20Shot%202023-01-03%20at%2010.29.35%20PM.png)
 
 ---
 
 <p align="middle">(<a href="#readme-top">back to top</a>)</p>
 
-
 ## Access
 
-   Here is the link to the repository https://github.com/BogartDME/crowd-surf-music-app
+Here is the link to the repository https://github.com/BogartDME/crowd-surf-music-app
 
-
-   Here is the link to the deployed application https://bogartdme.github.io/crowd-surf-music-app/
-  
-
-
-
+Here is the link to the deployed application https://bogartdme.github.io/crowd-surf-music-app/
 
 ## Usage
 
-  The user will search an artist they are interested in and the app will show them their top five songs on Spotify. In addition the user will be presented with ticketmasters upcoming events for the searched artist. The user will be able to make notes about the artist at the bottom and save the notes. 
-
-
-
+The user will search an artist they are interested in and the app will show them their top five songs on Spotify. In addition the user will be presented with ticketmasters upcoming events for the searched artist. The user will be able to make notes about the artist at the bottom and save the notes.
 
 ## Contributors
 
@@ -79,15 +59,13 @@
 - [Jason White](https://github.com/JasonAdalWhite)
 - [Edwin Pietrowski](https://github.com/BogartDME)
 
+## Featured Contributors
 
-
+- [Dylan Freeman](https://github.com/templarmanatee) - Assisted with debugging our Spotify API (Thank you Dylan!)
 
 ## Contact
 
-  If you would like to contact us with suggestions or would like to collaborate on a project feel free to contact us at our githubs linked above.
-
-
-
+If you would like to contact us with suggestions or would like to collaborate on a project feel free to contact us at our githubs linked above.
 
 ## Future Features
 
@@ -97,8 +75,4 @@
 - Song lyrics
 - Genre selection
 
-
-
-
   <p align="middle">(<a href="#readme-top">back to top</a>)</p>
-
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,63 +1,55 @@
-var artist; 
-// function getBoredResponse() {
-//   // chart.artists.get?page=1&page_size=3&country=it
-
-//   // var spotifyAuthToken = 'BQA6Ibri0327o0YIhdgBapB8VvZ6CgJxoaz_84iXy9iF65Hz4dEg2eaElf7NJYEgSCeXFD9aZ0J2a2cuEodWYgbh3q0PlUQ09hMx7R2GdjCvvFvEziKzmKkYOuMp2IN-UlynME7e4Ayr19XObNbLY7zNevsmD90-tDCpSzGUeSOrNCA';
-  var requestUrl = 'http://www.boredapi.com/api/activity?participants=1';
-  
-  // AJAX call requires a third party library, jQuery
+var artist;
+//Global variables to store user IDs
+const authToken = getAccessTokenFromUrl();
+// ticketmaster API
+$("#artist-search").on("click", function () {
+  artist = $("#artist-input").val();
+  getTracks(artist);
   $.ajax({
     type: "GET",
-    
-    url: requestUrl,
+    //when spotify api is up this api will be dependent on pulling the performer to search their events as such:
+    url:
+      "https://app.ticketmaster.com/discovery/v2/events.json?keyword=" +
+      artist +
+      "-&countryCode=US&size=10&apikey=hMNJM8tD1PpcNAUBm6ZArJQt8MPJUpWy",
     async: true,
-    dataType: "json", 
+    dataType: "json",
     success: function (json) {
-      console.log(json.activity, json.type, json.participants);
-    }
-    //   console.log(response.status);
-  });
-  // ticketmaster API
-  $("#artist-search").on("click", function(){
-    artist = $("#artist-input").val();
-    $.ajax({
-        type: "GET",
-        //when spotify api is up this api will be dependent on pulling the performer to search their events as such:
-        // "https://app.ticketmaster.com/discovery/v2/events.json?keyword=Adele-&countryCode=US&size=10&apikey=hMNJM8tD1PpcNAUBm6ZArJQt8MPJUpWy"
-        url: "https://app.ticketmaster.com/discovery/v2/events.json?keyword=" + artist + "-&countryCode=US&size=10&apikey=hMNJM8tD1PpcNAUBm6ZArJQt8MPJUpWy",
-        async: true,
-        dataType: "json",
-        success: function (json) {
-            console.log(json._embedded.events);
-            let i = 0;
-            for (const showInfo of json._embedded.events) {
-                if (i < 5) {
-                    let eventName = showInfo.name;
-                    console.log(eventName);
-                    let eventDate = showInfo.dates.start.localDate;
-                    let eventTime = showInfo.dates.start.localTime;
-                    let contentHTML = `
-                <div class="p-4 card text-lg">
-                <h3>Tour Name: ${eventName}</h3>
-                <p>Date: ${eventDate}</p>
-                <p>Time: ${eventTime}</p>
-                </div>
+      console.log(json._embedded.events);
+      let i = 0;
+      $("#song-detail").empty()
+      for (const showInfo of json._embedded.events) {
+        if (i < 5) {
+          let eventName = showInfo.name;
+          console.log(eventName);
+          let eventDate = showInfo.dates.start.localDate;
+          let eventTime = showInfo.dates.start.localTime;
+          let contentHTML = `
+                <div class="flex text-center justify-center p-5">
+    <div class="block p-6 rounded-lg shadow-lg bg-gray-300 max-w-md">
+      <h5 class="text-teal-500 text-xl leading-tight font-medium mb-2">
+        Tour Name: ${eventName}
+      </h5>
+      <p class="text-teal-700 text-lg mb-4">Date: ${eventDate}</p>
+      <p class="text-teal-700 text-lg mb-4">Time: ${eventTime}</p>
+    </div>
+  </div>
                 `;
-                    $("#song-detail").append(contentHTML);
-                    i++;
-                }
-            }
-            // Parse the response.
-        },
-        error: function (xhr, status, err) {},
-    });
+          $("#song-detail").append(contentHTML);
+          i++;
+        }
+      }
+      // Parse the response.
+    },
+    error: function (xhr, status, err) {},
+  });
 
-// save comments to local storage
-$("#save-btn").on("click", function () {
+  // save comments to local storage
+  $("#save-btn").on("click", function () {
     var userInput = $(this).siblings("#user-notes").val();
     var userDesc = $(this).parent().attr("id");
     localStorage.setItem(userDesc, userInput);
-})
+  });
 });
 
 $("#song-detail-2 #user-notes").val(localStorage.getItem("song-detail-2"));
@@ -71,27 +63,84 @@ let btn = document.getElementById("instruct-modal");
 let button = document.getElementById("close-modal");
 
 // Will open modal when clicked
-btn.onclick = function() {
-    modal.style.display = "block";
-    }
+btn.onclick = function () {
+  modal.style.display = "block";
+};
 
-    // Will close modal when clicked
-button.onclick = function() {
-    modal.style.display = "none";
-    }
+// Will close modal when clicked
+button.onclick = function () {
+  modal.style.display = "none";
+};
 
 // Will close the modal if the user clicks outside of it
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }}
-        
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
 
+// Spotify API
+const searchArtist = async (artistString) => {
+  const url = `https://api.spotify.com/v1/search?q=${artistString}&type=artist&limit=1`;
+  const response = await fetch(url, {
+    method: "GET",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+      Authorization: "Bearer " + authToken,
+    },
+  });
+  const artistData = await response.json();
+  console.log(artistData.artists.items[0].id);
+  return await artistData.artists.items[0].id;
+};
 
+const getTracks = async (artist) => {
+  const artistId = await searchArtist(artist);
+  const url = `https://api.spotify.com/v1/artists/${artistId}/top-tracks?market=US`;
+  const response = await fetch(url, {
+    method: "GET",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+      Authorization: "Bearer " + authToken,
+    },
+  });
+  const artistTopTracks = await response.json();
+  console.log(artistTopTracks);
+  // update html
+  for (let index = 0; index < 5; index++) {
+    const element = artistTopTracks.tracks[index];
+    $(`#song-${index + 1}`).text(element.name)
+  }
+  return artistTopTracks;
+};
 
+// Spotify Auth
+// Creates Spotify authentication link, redirects there, and sends the user back
+function spotifyAuth() {
+  //Application ID for Spotify
+  var client_id = "c606a7901ceb4201a0fd0ae55d3b4831";
+  var redirect_uri = "http://127.0.0.1:5500/";
 
+  //Permissions
+  var scope = "user-read-private user-read-email";
 
-
-
-// getBoredResponse();
-
+  //Builds authentication URL
+  var url = "https://accounts.spotify.com/authorize";
+  url += "?response_type=token";
+  url += "&client_id=" + encodeURIComponent(client_id);
+  url += "&scope=" + encodeURIComponent(scope);
+  url += "&redirect_uri=" + encodeURIComponent(redirect_uri);
+  window.location.replace(url);
+}
+// Gets full hash info from URL and splits out the access token
+function getAccessTokenFromUrl() {
+    if (!window.location.hash) {
+        spotifyAuth();
+    }
+    var hash = window.location.hash.substring(1);
+    var hash1 = hash.split('=')[1];
+    var hash2 = hash1.split('&')[0];
+    return hash2;
+}
